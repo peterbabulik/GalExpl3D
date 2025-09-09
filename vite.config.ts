07b5f81876920bc0504b11e26bf14a75afd96d32
pe3k <pe3k@T4s.home>
2025-09-09 12:29:29 +0200

    fix of vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import path from 'path';
+import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // This line is the critical fix
+      base: '/GalExpl3D/', 
+      
+      plugins: [react()], 
+      
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
